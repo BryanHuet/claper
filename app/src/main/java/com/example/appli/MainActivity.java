73cd500673ac1723d5333553555928f7935ed8b0
Tolga <tolgatrr61@gmail.com>
2021-01-19 17:42:45 +0100

    photo

--- app/src/main/java/com/example/appli/MainActivity.java
+++ app/src/main/java/com/example/appli/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.appli;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Initialisation des attributs.
+    ImageView imageView;
+    Button btOpen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialisation des attributs;
+        imageView = findViewById(R.id.image_view);
+        btOpen = findViewById(R.id.bt_open);
+
+        // Requête pour la permission caméra
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {
+                    Manifest.permission.CAMERA
+            },
+                    100);
+        }
+
+        // Listener sur le bouton.
+        btOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ouvre la Camera
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    // Résultat de l'activité (on récupère l'image et la met dans l'ImageView).
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            // Get l'image Capturé
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            // Set l'image capturé  dans l'ImageView (là il faudrait lancer l'IA et rediriger vers une nouvelle activité résultant de celle-ci)
+            imageView.setImageBitmap(captureImage);
+        }
     }
 }
