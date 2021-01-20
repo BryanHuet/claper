87daa845e5a68cf8f6cbddc5f73ff82954117e37
Tolga <tolgatrr61@gmail.com>
2021-01-20 16:54:21 +0100

    Maj pendant cette magnifique pause.

--- app/src/main/java/com/example/appli/FourthActivity.java
+++ app/src/main/java/com/example/appli/FourthActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+//TODO Réfléchir sur la gestion et la sauvegarde des états entre les différentes redirection + la liaison des parents de l'activity 4 qui va être le fils de 1 et de 3.
+
+//TODO liste des films recherchés (save sur un fichier json chaque film trouvé)
 public class FourthActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/example/appli/SecondActivity.java
+++ app/src/main/java/com/example/appli/SecondActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,10 +24,23 @@ public class SecondActivity extends AppCompatActivity {
         // Initialisation des attributs;
         imageView = findViewById(R.id.image_result);
         btDescription = findViewById(R.id.btn_description);
+        /* TODO Plus nécessaire de tester les affichages des images importer/prise, je vais faire la partie db et faker l'affichage d'images pré-sauvegarder.
+        String previousActivity = "ThirdActivity";
+        if (getParentActivityIntent() != null) {
+            previousActivity = (String) getIntent().getExtras().getString("fromActivity");
+        }
 
-        // On récupère l'image passé en argument dans la redirection de la 1ère activity.
-        Bitmap passedImage = (Bitmap) getIntent().getParcelableExtra("bmp_img");
-        imageView.setImageBitmap(passedImage);
+
+        if (previousActivity.equals("MainActivity")) {
+            // On récupère l'image passé en argument dans la redirection de la 1ère activity.
+            Bitmap passedImage = (Bitmap) getIntent().getParcelableExtra("bmp_img");
+            String pathFile = (String) getIntent().getExtras().getString("filePath");
+            if (passedImage != null) {
+                imageView.setImageBitmap(passedImage);
+            } else imageView.setImageBitmap(BitmapFactory.decodeFile(pathFile));
+        }
+         */
+
 
         // Clicklistener sur le bouton qui redirige vers une 3ème activity.
         btDescription.setOnClickListener(new View.OnClickListener() {
